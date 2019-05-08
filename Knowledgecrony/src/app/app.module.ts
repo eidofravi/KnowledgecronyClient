3f5cea33eb731e2d88d75f1e59bfc3bb424c7702
ravi123 <ravi123@192.168.1.8>
2019-05-07 20:22:19 -0400

    create a new show-answer component

--- Knowledgecrony/src/app/app.module.ts
+++ Knowledgecrony/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShowAnswerComponent } from './show-answer/show-answer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowAnswerComponent
   ],
   imports: [
     BrowserModule,
